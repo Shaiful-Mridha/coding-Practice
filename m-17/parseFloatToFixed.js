e2bcc3dcf9838a80b5215445023909279fd7e16e
Shaiful Mridha <shaifuldu555@gmail.com>
2023-10-31 12:34:32 +0200

    first commit

---
+++ m-17/parseFloatToFixed.js
@@ -0,0 +1,11 @@
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+//I can fix how many digits I want after decimals using toFixed method.
+var sum = sum.toFixed(1);
+// after using toFixed method now it is string, so I need to parseFloat
+sum = parseFloat(sum);
+console.log(sum);
+console.log(typeof sum);
+console.log(num1);
+console.log(typeof num1);
---
+++ m-17/practice.js
@@ -0,0 +1,28 @@
+var money = 1000;
+var orange = 300;
+var apple = 400;
+var cost = orange + apple;
+var remaining = money - cost;
+// console.log(remaining);
+
+var math = 75.25;
+var eng = 65;
+var phy = 80;
+var bio = 35.45;
+var bang = 99.5;
+var sum = math + eng + phy + bio + bang;
+sum = sum / 5;
+sum = sum.toFixed(2);
+sum = parseFloat(sum);
+// console.log(sum);
+// console.log(typeof sum);
+
+var firstLine = "I am going to be";
+var secondLine = "an awesome web developer.";
+var result = firstLine + " " + secondLine;
+// console.log(result);
+
+var number = 119;
+var num = 5;
+var result = number % num;
+console.log(result)
