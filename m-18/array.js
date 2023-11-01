c1a362033f87e6f9eb746932da55860f0882c48d
Shaiful Mridha <shaifuldu555@gmail.com>
2023-11-01 09:59:36 +0200

    array added

--- m-18/array.js
+++ m-18/array.js
@@ -1,5 +1,6 @@
 var prices = [24, 45, 36, 78, 90];
 // console.log(prices);
+// console.log(prices.length)
 
 var element = prices[3];
 // console.log(element);
@@ -10,3 +11,25 @@ var position = prices.indexOf(45);
 // set value in element
 prices[3] = 77;
 // console.log(prices);
+
+// using pop() push()
+
+var friends = ["Sam", "Brain", "Tyson", "Peterson"];
+// console.log(friends);
+// friends.push("James");
+// console.log(friends);
+
+var player = ["Messy", "Rolando", "Chris", "Ben"];
+var element = player.push("Root");
+// console.log(element);
+// console.log(player);
+
+var foods = ["apple", "orange", "banana"];
+// console.log(foods);
+// foods.pop();
+// console.log(foods);
+
+var cities = ["tallinn", "helsinki", "stockholm", "riga"];
+var element = cities.pop();
+// console.log(cities);
+// console.log(element);
