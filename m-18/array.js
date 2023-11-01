69567c18ec94f81447080be2ec30d9154108ae02
Shaiful Mridha <shaifuldu555@gmail.com>
2023-11-01 09:38:08 +0200

    updated

---
+++ m-18/array.js
@@ -0,0 +1,12 @@
+var prices = [24, 45, 36, 78, 90];
+// console.log(prices);
+
+var element = prices[3];
+// console.log(element);
+
+var position = prices.indexOf(45);
+// console.log(position);
+
+// set value in element
+prices[3] = 77;
+// console.log(prices);
--- m-18/first.js
+++
@@ -1,7 +0,0 @@
-var num = 10.57;
-var value = 1.9798;
-var sum = num + value;
-sum = sum.toFixed(3);
-sum = parseFloat(sum);
-
-console.log(sum);
